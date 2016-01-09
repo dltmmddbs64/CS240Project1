a14f3f644a69afa81e6d8f16aaef360ddaed93c5
Seungyun Lee <sylee714@gmail.com>
2016-01-08 20:47:00 -0800

    Initial test

---
+++ src/cpp/edu/cs/cs240/project1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cpp.edu.cs.cs240.project1;
+
+/**
+ * @author Seungyun
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/cpp/edu/cs/cs240/project1/UserInterface.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package cpp.edu.cs.cs240.project1;
+
+/**
+ * @author Seungyun
+ *
+ */
+public class UserInterface {
+
+}
